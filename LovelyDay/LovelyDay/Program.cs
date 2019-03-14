304645806ad57f3e0d53549628c69993c2670d62
Elias Öh <elias.oh@outlook.com>
2019-03-14 15:31:58 +0100

    Gotta commit the right project, silly me.

--- LovelyDay/LovelyDay/Program.cs
+++ LovelyDay/LovelyDay/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LovelyDay2_0
+namespace LovelyDay
 {
     class Program
     {
--- LovelyDay/LovelyDay/Properties/AssemblyInfo.cs
+++ LovelyDay/LovelyDay/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LovelyDay2.0")]
+[assembly: AssemblyTitle("LovelyDay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LovelyDay2.0")]
+[assembly: AssemblyProduct("LovelyDay")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("efcd11a8-8393-4101-a53d-b429921c0d36")]
+[assembly: Guid("f126891a-4d08-431e-9841-cdb448ca797f")]
 
 // Version information for an assembly consists of the following four values:
 //
