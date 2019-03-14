a7606f0bd8c79fea3f64e3683a923b454d08f4da
Elias Öh <elias.oh@outlook.com>
2019-03-14 18:23:08 +0100

    Adding Timer class and modified Program class.

--- LovelyDay/LovelyDay/Program.cs
+++ LovelyDay/LovelyDay/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace LovelyDay
 {
     class Program
     {
+        private static bool isRunning = true;
+
         static void Main(string[] args)
         {
+            while (isRunning)
+            {
+                Update(0f /*Send DeltaTime*/ );
+            }
+        }
+
+        static void Update(float aDeltaTime)
+        {
+            
+        }
+
+        public static void Kill()
+        {
+            isRunning = false;
         }
     }
 }
