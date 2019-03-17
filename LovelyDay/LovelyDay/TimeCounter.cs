c477317dd2e6957793ba3f1a58186efacc687c81
SpaceLenore <libreterra@protonmail.com>
2019-03-17 20:10:47 +0100

    Added timeCounter Class

---
+++ LovelyDay/LovelyDay/TimeCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace LovelyDay
+{
+	class TimeCounter
+	{
+		//Should this class be re-written to be a thread instead?
+
+		private float timeToCount;
+		private float startTime;
+
+		public TimeCounter (float usrTime)
+		{
+			timeToCount = usrTime;
+			startTime = Timer.GetTotalTime ();
+		}
+
+		public bool blockingUpdateChecker () {
+			while (!noBlockCheck()) {
+				Timer.Update ();
+				continue;
+			}
+			return true;
+		}
+
+		public bool noBlockCheck() {
+			if (Timer.GetTotalTime() - startTime >= timeToCount) {
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
